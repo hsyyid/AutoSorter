abf30c8d9e5d0ee998de16f0a6b336d77894353d
Calvin Godfrey <calvinbbcard@gmail.com>
2019-03-02 16:58:44 -0500

    Can drive?

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
+const {google} = require('googleapis');
+
 exports.helloHttp = (req, res) => {
-  res.send(`Hello ${(req.query.query)}`);
-  console.log(JSON.stringify(req));
+  console.log(req.body);
+  const {token} = req.body;
+  // TODO: Get OAuth2 token
+  res.send(listFiles(token));
+}
+
+/**
+ * Lists the names and IDs of up to 10 files.
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function listFiles(auth) {
+  const drive = google.drive({version: 'v3', auth});
+  drive.files.list({
+    pageSize: 100,
+    fields: 'nextPageToken, files(id, name, mimeType)',
+  }, (err, res) => {
+    if (err) res.send('The API returned an error: ' + err);
+    let ans = "";
+    const files = res.data.files;
+    if (files.length) {
+      ans += "Files:\n";
+      files.map((file) => {
+        ans += `${file.name} (${file.id}) - ${file.mimeType}\n`
+      });
+    } else {
+      ans += "No files found.";
+    }
+    return ans;
+  });
 }
