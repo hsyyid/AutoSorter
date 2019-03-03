14c0079e0dad28b5b1c210c29024012a910577e9
Hassan Syyid <hassansyyid@gmail.com>
2019-03-02 22:44:29 -0500

    asd

--- drive.py
+++ drive.py
@@ -3,30 +3,36 @@ from oauth2client.client import AccessTokenCredentials
 
 
 def get_text(access_token, file_ids):
-    creds = AccessTokenCredentials(access_token, 'my-user-agent/1.0')
-    drive_service = build('drive', 'v3', credentials=creds)
-    response = []
-    should_return = False
+    text = []
 
-    def callback(request_id, text, exception):
-        if exception:
-            # Handle error
-            print(exception)
-        else:
-            print(response)
-            response.append(text)
+    try:
+        creds = AccessTokenCredentials(access_token, 'my-user-agent/1.0')
+        drive_service = build('drive', 'v3', credentials=creds)
 
-    batch = drive_service.new_batch_http_request(callback=callback)
+        def callback(request_id, response, exception):
+            if exception:
+                # Handle error
+                print(exception)
+            else:
+                print(response)
+                text.append(response)
 
-    for file_id in file_ids:
-        batch.add(drive_service.files().export(
-            fileId=file_id,
-            mimeType="text/plain",
-            fields='id',
-        ))
-    pass
+                if len(text) == len(file_ids):
+                    raise Exception('test')
 
-    batch.execute()
+        batch = drive_service.new_batch_http_request(callback=callback)
+
+        for file_id in file_ids:
+            batch.add(drive_service.files().export(
+                fileId=file_id,
+                mimeType="text/plain",
+                fields='id',
+            ))
+        pass
+
+        batch.execute()
+    except:
+        return text
 
     # while len(response) != len(file_ids):
     #     print(len(response))
